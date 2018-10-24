f0175cde7653c2114af4cadc761ca44f4d975b2b
洪泽权 <625594687@qq.com>
2018-10-24 23:22:48 +0800

    1.完善登录界面页面 2.个人系统，等级提升页面 3.处理好页面布局规划问题

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // 这里放置一些公用的方法
+$('.j-tab li').click(function(){
+	$(this).addClass('active').siblings().removeClass('active');
+	$('.j-item .list').eq($(this).index()).show().siblings().hide();
+})
