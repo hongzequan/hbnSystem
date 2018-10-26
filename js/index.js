39c5ff9601ba8e0e4c4886162a51cda8a723d3bb
洪泽权 <625594687@qq.com>
2018-10-26 21:24:41 +0800

    分享素材页面及拷贝文字功能

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 // 这里放置一些公用的方法
-$('.j-tab li').click(function(){
-	$(this).addClass('active').siblings().removeClass('active');
-	$('.j-item .list').eq($(this).index()).show().siblings().hide();
+$('.j-tab li').click(function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.j-item .list').eq($(this).index()).show().siblings().hide();
 })
+
+// 点击拷贝文字
+function setCopyText(obj) {
+    var id = '#' + $(obj).attr('id');
+    var clipboard = new Clipboard(id);
+    clipboard.on('success', function(e) {
+        alert("复制成功", 1500);
+        e.clearSelection();
+        clipboard.destroy();
+    });
+    clipboard.on('error', function(e) {
+        alert("当前浏览器不支持此功能，请手动复制。")
+    });
+}
